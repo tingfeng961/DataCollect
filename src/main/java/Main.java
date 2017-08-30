c33407d0d0707cf4c4333524b89e91635d09b5ec
tingfeng961 <1582091411@qq.com>
2017-08-30 14:24:56 +0800

    add log

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by Administrator on 2017/8/29.
  */
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("data collect!");
+        logger.debug("debug");
+        logger.error("error");
+        logger.info("info");
+        logger.warn("warn");
+        logger.trace("trace");
     }
 }
